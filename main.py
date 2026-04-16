6503186338771d10dd1a872a7e8a332b91e89246
Deebing <artemevdakov08@gmail.com>
2026-04-16 20:27:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ import websockets
 
 clients = set()
 
-async def handler(websocket):
-    clients.add(websocket)
-    print("Client connected")
+rooms = {}
 
-    try:
-        async for message in websocket:
-            # пересылаем всем
-            for client in clients:
-                if client != websocket:
-                    await client.send(message)
-    except:
-        pass
-    finally:
-        clients.remove(websocket)
-        print("Client disconnected")
+async def handler(websocket):
+    async for message in websocket:
+        data = json.loads(message)
+
+        room = data["room"]
+        text = data["text"]
+
+        if room not in rooms:
+            rooms[room] = set()
+
+        # добавляем клиента в комнату
+        rooms[room].add(websocket)
+
+        # пересылаем только в эту комнату
+        for client in rooms[room]:
+            await client.send(json.dumps(data))
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", 10000):
