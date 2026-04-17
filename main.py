fb25dee610ab1c443abcb7a039b5fa875a418750
Deebing <artemevdakov08@gmail.com>
2026-04-17 16:34:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,39 @@ import os
 rooms = {}
 
 async def handler(websocket):
-    async for message in websocket:
-        data = json.loads(message)
+    print("CLIENT CONNECTED")
 
-        room = data.get("room")
-        text = data.get("text")
+    try:
+        async for message in websocket:
+            data = json.loads(message)
 
-        if room not in rooms:
-            rooms[room] = set()
+            room = data.get("room")
+            text = data.get("text")
 
-        rooms[room].add(websocket)
+            if room not in rooms:
+                rooms[room] = set()
 
-        for client in list(rooms[room]):
-            try:
-                await client.send(json.dumps(data))
-            except:
-                rooms[room].remove(client)
+            rooms[room].add(websocket)
+
+            for client in list(rooms[room]):
+                try:
+                    await client.send(json.dumps(data))
+                except:
+                    rooms[room].remove(client)
+
+    except:
+        print("CLIENT DISCONNECTED")
 
 async def main():
     port = int(os.environ.get("PORT", 10000))
 
-    print("SERVER STARTED", port)
+    print("SERVER STARTED ON", port)
+
+    async with websockets.serve(handler, "0.0.0.0", port):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
     async with websockets.serve(handler, "0.0.0.0", port):
         await asyncio.Future()
